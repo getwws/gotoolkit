19797a4930dcbd75a95095987d89ca801b3c009b
allen <admin@h1cms.com>
2019-07-07 11:47:28 +0800

    string util

--- strings.go
+++ strings.go
@@ -1,1 +1,23 @@
 package gotoolkit
+
+func isUpperASCII(c byte) bool {
+	return 'A' <= c && c <= 'Z'
+}
+
+func isLowerASCII(c byte) bool {
+	return 'a' <= c && c <= 'z'
+}
+
+func toUpperASCII(c byte) byte {
+	if isLowerASCII(c) {
+		return c - ('a' - 'A')
+	}
+	return c
+}
+
+func toLowerASCII(c byte) byte {
+	if isUpperASCII(c) {
+		return c + 'a' - 'A'
+	}
+	return c
+}
